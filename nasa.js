fa4002e2f360130c89ecc6a728051f9580003096
Kathyayini12 <128512405+Kathyayini12@users.noreply.github.com>
2026-02-04 16:51:28 -0600

    Update environment variable for NASA API key

--- nasa.js
+++ nasa.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const API_KEY = process.env.YOUR_API_KEY;
+const API_KEY = process.env.NASA_API_KEY;
 const START_DATE = '2025-01-01';
 const END_DATE = '2025-01-07';
 
